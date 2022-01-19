95b589141f60444ab224650a35b68f5ba13044a7
ArthurExcalibur <1481891318@qq.com>
2022-01-19 10:48:05 +0800

    navigation

--- app/src/main/java/com/excalibur/enjoylearning/jetpack/navigation/Fragment2.kt
+++ app/src/main/java/com/excalibur/enjoylearning/jetpack/navigation/Fragment2.kt
@@ -1,6 +1,22 @@
 package com.excalibur.enjoylearning.jetpack.navigation
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.excalibur.enjoylearning.R
 
 class Fragment2 : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Log.e("TestForCase", "Fragment2 onCreateView")
+        return layoutInflater.inflate(R.layout.fra_nav2, null)
+    }
+
 }
--- app/src/main/java/com/excalibur/enjoylearning/jetpack/navigation/NavigationActivity.kt
+++ app/src/main/java/com/excalibur/enjoylearning/jetpack/navigation/NavigationActivity.kt
@@ -1,21 +1,38 @@
 package com.excalibur.enjoylearning.jetpack.navigation
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.excalibur.enjoylearning.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NavigationActivity : AppCompatActivity() {
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.NO_DEBUG) as NavHostFragment ?
-//
-//        val controller = navHostFragment?.navController
-//
-//        controller?.
-//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_main)
+        // fragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment ?
+        val controller = navHostFragment!!.navController
+
+        //bottom ui
+        val bottom: BottomNavigationView? = findViewById(R.id.nav_view)
+
+        //bind
+        bottom?.setupWithNavController(controller)
+//        NavigationUI.setupWithNavController(bottom!!, controller)
+//        bottom?.setOnNavigationItemSelectedListener { menuItem ->
+//            // 导航到与menuItem菜单项关联的NavDestination，即与menu.xml中item的id相同的destinationId
+//            // destinationId即navigation/nav_graph.xml中fragment的id
+//            NavigationUI.onNavDestinationSelected(menuItem, controller)
+//        }
+    }
 
 }
